5aae06e83c25e67d05ac9872e067ec1d5c8aff52
manoj <manojk.mukka@gmail.com>
2017-03-28 21:15:50 +0000

    added new cookbook

---
+++ cookbooks/custom_apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'custom_apache'
+maintainer       'manoj company'
+maintainer_email 'manojk.mukka@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures custom_apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
